223a9e3ceed3744e99ade8aa59bb144d1a4f4aa6
Alen <alivojevic@riteh.hr>
2021-04-21 17:55:15 +0200

    test level map

---
+++ core/assets/TileAssets/TileAssets/Maps/Space.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="Space" tilewidth="64" tileheight="64" tilecount="480" columns="30">
+ <image source="../Space.jpg" width="1920" height="1080"/>
+</tileset>
---
+++ core/assets/TileAssets/TileAssets/Maps/Test.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="Test" tilewidth="64" tileheight="64" tilecount="540" columns="30">
+ <image source="../cave4000.png" width="1920" height="1200"/>
+</tileset>
