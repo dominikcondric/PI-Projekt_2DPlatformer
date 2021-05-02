7180196642082f6fe8a9038a309483b94ca01e29
Dominik <dcondric@riteh.hr>
2021-05-02 19:33:49 +0200

    Added Cave map class and game over screen

---
+++ core/src/scenes/CaveScene.java
@@ -0,0 +1,53 @@
+package scenes;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+
+import entities.Enemy;
+
+public class CaveScene extends Scene {
+
+	public CaveScene(TmxMapLoader mapLoader, SpriteBatch batch) {
+		super(mapLoader, "Cave/Maps/demo3.tmx", batch);
+	}
+
+	@Override
+	public void constructTileMap() {
+		BodyDef bodyDef = null;
+		PolygonShape shape = null;
+		FixtureDef fixtureDef = null;
+		Body body = null;
+		float scalingFactor = 1f / map.getProperties().get("tilewidth", Integer.class);
+		for (MapObject object : map.getLayers().get(2).getObjects().getByType(RectangleMapObject.class)) {
+			Rectangle rect = ((RectangleMapObject)object).getRectangle();
+			rect.set(rect.getX() * scalingFactor, rect.getY() * scalingFactor, rect.getWidth() * scalingFactor, rect.getHeight() * scalingFactor);
+			bodyDef = new BodyDef();
+			bodyDef.type = BodyDef.BodyType.StaticBody;
+			bodyDef.position.set(rect.getX() + rect.getWidth() / 2f, rect.getY() + rect.getHeight() / 2f);
+			
+			body = box2DWorld.createBody(bodyDef);
+			shape = new PolygonShape();
+			shape.setAsBox(rect.getWidth() / 2f, rect.getHeight() / 2f);
+			fixtureDef = new FixtureDef();
+			fixtureDef.shape = shape;
+			fixtureDef.friction = 0.f;
+			
+			body.createFixture(fixtureDef);
+			shape.dispose();
+		}
+	}
+
+
+	@Override
+	public void constructEntities() {
+		addEntity(new Enemy(new Vector2(5.f, 8.f)));
+	}
+}
---
+++ core/src/screens/GameOverScreen.java
@@ -0,0 +1,140 @@
+package screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Pixmap.Format;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Cell;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.platformer.Platformer;
+
+public class GameOverScreen implements Screen {
+	final Platformer game;
+	private Stage userInterface;
+	private Table table;
+	private final GameScreen gameScreen;
+	private TextButton continueGame;
+	private TextButton exitGame;
+	public enum ScreenType { GAME_OVER };
+	
+	public GameOverScreen(final Platformer game, final GameScreen gameScreen, ScreenType type) {
+		this.game = game;
+		this.gameScreen = gameScreen;
+		
+		userInterface = new Stage(new FitViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()), game.batch);
+		table = new Table();
+		
+		Gdx.input.setInputProcessor(userInterface);
+		
+		// Setup
+		table = new Table();
+		userInterface.addActor(table);
+		table.setFillParent(true);
+
+		table.align(Align.center);
+		float buttonHeight = userInterface.getHeight() / 8;
+		
+		Pixmap pixmap = new Pixmap(1, 1, Format.RGB888);
+		pixmap.setColor(Color.BROWN);
+		pixmap.fill();
+		TextureRegionDrawable texture = new TextureRegionDrawable(new Texture(pixmap));
+		
+		TextButtonStyle style = new TextButtonStyle();
+		style.font = game.font;
+		style.fontColor = Color.WHITE;
+		style.overFontColor = Color.SKY;
+		style.up = texture;
+		
+		String s = new String();
+		if (type == ScreenType.GAME_OVER) {
+			s = "Game over";
+		}
+		
+		game.font.getData().setScale(4.f);
+		Label gameStateLabel = new Label(s, new Label.LabelStyle(game.font, Color.BROWN));
+		Cell<Label> labelCell = table.add(gameStateLabel);
+		labelCell.align(Align.center);
+		gameStateLabel.setAlignment(Align.center);
+		labelCell.width(userInterface.getWidth() / 2);
+		labelCell.height(buttonHeight);
+		table.row();
+		
+		continueGame = new TextButton("Continue game", style);
+		exitGame = new TextButton("Exit game", style);
+		
+		Cell<TextButton> cell;
+		cell = table.add(continueGame);
+		cell.spaceBottom(buttonHeight / 3.f);
+		cell.width(userInterface.getWidth() / 2);
+		cell.height(buttonHeight);
+
+		table.row();
+		cell = table.add(exitGame);
+		cell.spaceBottom(buttonHeight / 3.f);
+		cell.width(userInterface.getWidth() / 2);
+		cell.height(buttonHeight);
+		cell.fill();
+		
+		continueGame.getLabel().setFontScale(buttonHeight / 20.f);
+		exitGame.getLabel().setFontScale(buttonHeight / 20.f);
+	}
+	
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render(float delta) {
+		ScreenUtils.clear(Color.BLACK);
+		userInterface.act();
+		userInterface.draw();
+		if (continueGame.getClickListener().isPressed()) {
+			game.setScreen(gameScreen);
+		} else if (exitGame.getClickListener().isPressed()) {
+			game.setScreen(new MainMenuScreen(game));
+		}
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
