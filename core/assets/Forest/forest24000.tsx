c74bc5a67950401cd0f3871fb18c40715ab6b09a
Alen <alivojevic@riteh.hr>
2021-05-03 10:32:55 +0200

    Forest map added, Snow background; close #20

---
+++ core/assets/Forest/forest24000.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="forest24000" tilewidth="32" tileheight="32" tilecount="10000" columns="125">
+ <image source="forest24000.jpeg" width="4000" height="2560"/>
+</tileset>
